ee356ab668bd2c11b905ff9898b4815a75877e0d
eric2788 <tsukiko852@gmail.com>
2020-11-05 18:34:44 +0800

    init commit

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import commonjs from 'rollup-plugin-commonjs';
+import resolve from 'rollup-plugin-node-resolve';
+import copy from 'rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+    input: 'src/index.js',
+    output: {
+      dir: './dist',
+      format: 'cjs'
+      /* firefox extension not support chunk splitting :(
+      manualChunks(id) {
+        if (id.includes('node_modules')) {
+          return 'vendor';
+        }
+      }
+      */
+    },
+    watch: {
+        include: 'src/**',
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        copy({
+            targets: [
+                { src: 'src/manifest.json', dest: 'dist'},
+                { src: 'src/icons/*', dest: 'dist/icons'}
+            ]
+        }),
+        terser()
+    ]
+  };
---
+++ src/index.js
@@ -0,0 +1,103 @@
+'use strict';
+import $ from 'jquery'
+import * as signalR from '@microsoft/signalr'
+
+let roomId = location.pathname.substring(1)
+
+async function validate(){
+    const id = parseInt(roomId)
+    if (isNaN(id)) {
+        throw error('this is not living room')
+    }
+    console.log('fetching vup api')
+    const data = await fetch('https://vup.darkflame.ga/api/online').then(r => r.json())
+    console.log('fetched successful')
+    const roomIds = data.list.map(s => s.roomId)
+    return roomIds.includes(id)
+}
+
+
+
+
+function insertViewerDom(){
+    const ele = $('div.upper-right-ctnr.p-absolute.none-select')
+    ele.append(`
+        <div style="color: gray" title="已知互動人數" class="right-action-ctnr dp-i-block">
+            <i class="icon-font icon-view live-skin-normal-text v-middle"></i>
+            <span class="action-text v-middle live-skin-normal-text dp-i-block" id="stream-viewer">--</span>
+        </div>
+        <div style="color: gray" title="真實彈幕數" class="right-action-ctnr dp-i-block">
+            <i style="color: gray; font-size: 15px" class="icon-font live-icon-danmaku-on live-skin-normal-text v-middle"></i>
+            <span class="action-text v-middle live-skin-normal-text dp-i-block" id="stream-danmaku">--</span>
+            (<span id="stream-viewer-danmaku">--</span>人)
+        </div>
+    `)
+    const popularEle = $('div[title=人气值]')
+    popularEle.append(`
+        (最高: <span id="stream-highest-popular">--</span>)
+    `)       
+    console.log(`room id is ${roomId}`)
+}
+
+const toDisplay =  (num) => num > 10000 ? num = (num / 10000).toFixed(1).concat("萬") : num
+
+const display = {
+    setEle: function(ele, num){
+        $(ele)[0].innerText = toDisplay(num)
+    },
+    setViewer: function(num){
+        this.setEle('#stream-viewer', num)
+    },
+    setDanmaku: function(num){
+        this.setEle('#stream-danmaku', num)
+    },
+    setDanmakuViewer: function(num){
+        this.setEle('#stream-viewer-danmaku', num)
+    },
+    setHighestPopular: function(num){
+        this.setEle('#stream-highest-popular', num)
+    }
+}
+
+
+// for vtuber
+async function startVupSignalR(){
+    const wss = `wss://vup.darkflame.ga/api/roomHub?roomId=${roomId}`
+    const connection = new signalR.HubConnectionBuilder()
+    .withUrl(wss, {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+    })
+    .withAutomaticReconnect([1000, 5000, 10000])
+    .build();
+    await connection.start();
+    console.log('signalR connected.')
+    connection.on("ReceiveRoomData", (_, data) => {
+        display.setViewer(data.participants)
+        display.setDanmaku(data.realDanmaku)
+        display.setDanmakuViewer(data.danmakuUser)
+        display.setHighestPopular(data.maxPopularity)
+    });
+    connection.onclose(err => {
+        console.warn(`web socket closed: ${err}`)
+    })
+    connection.onreconnected(() => console.log(`websocket reconnected...`))
+    connection.onreconnecting(() => console.log(`websocket reconnecting...`))
+}
+
+
+
+async function start(){
+    if (!await validate()) {
+        console.log('this live room is not virtual up, skipped')
+    }else{
+        console.log('this live room is virtual up, using vup.darkflame.ga')
+        insertViewerDom()
+        await startVupSignalR()
+    }
+}
+
+start().catch(err=>console.warn(`error happened: ${err}`))
+
+
+
